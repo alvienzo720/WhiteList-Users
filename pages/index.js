ccfa62d1ff983c99147cc2ddfe453e704e257c84
alvienzo720 <hudson1Dellmac>
2023-03-10 16:41:43 +0300

    Modify Index file

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+import { abi, WHITELIST_CONTRACT_ADDRESS } from "../constants";
+
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
